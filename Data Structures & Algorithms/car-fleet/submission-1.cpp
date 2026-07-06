6b78343e85eed2b2061417e4534bb4067798148e
AndriiMikita <103601417+AndriiMikita@users.noreply.github.com>
2026-07-06 23:09:55 +0300

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars = vector<pair<int, int>>(position.size());
+
+        for(int i = 0; i < speed.size(); ++i)
+            cars[i] = {position[i], speed[i]};
+
+        sort(cars.begin(), cars.end(), greater<>());
+
+        stack<pair<int, int>> st;
+
+        for(auto& [pos, speed] : cars)
+        {
+            if(st.empty())
+                st.push({pos, speed});
+
+            auto& [lpos, lspeed] = st.top();
+
+            if((target - lpos) * speed < (target - pos) * lspeed)
+                st.push({pos, speed});
+            
+        }
+
+        return st.size();
+    }
+};
